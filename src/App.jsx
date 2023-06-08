17a0bc5f8d5e9388bc3456e45dba815426ce4e0e
Oscar Miras <oscar.miras@gmail.com>
2023-06-08 11:49:13 +0200

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
+import foodsJson from "./foods.json";
+
 function App() {
+
+  // Create a state variable within the App component and store the foods array in it.
+  const [foods, setFoods] = useState([...foodsJson]);
+  console.log('Foods: ', foods);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
